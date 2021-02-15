88538d69a581ac6d746a0afeeea434febd1832d5
Fanks <hola@marm.dev>
2021-02-15 00:12:26 -0600

    First Commit

---
+++ server/src/routes/indexRoutes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { gamesController } from  '../controllers/gamesController';
+import { indexController } from '../controllers/indexController'
+
+class IndexRoutes {
+
+    public router: Router = Router();
+
+    constructor() {
+        this.config();
+    }
+
+    config() {
+        this.router.get('/', indexController.index);
+    }
+
+}
+
+const indexRoutes = new IndexRoutes();
+export default indexRoutes.router;
